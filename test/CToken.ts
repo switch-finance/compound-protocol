de07a4384023a664a8f2f737a026c8bfe1561e98
tercel <tercel.yi@gmail.com>
2021-12-28 18:27:33 +0800

    replace build-in tests and compile scripts

---
+++ test/CToken.ts
@@ -0,0 +1,165 @@
+import { Wallet, BigNumber } from 'ethers'
+import { ethers, network, waffle } from 'hardhat'
+import { Unitroller } from '../typechain/Unitroller'
+import { ComptrollerHarness } from '../typechain/ComptrollerHarness'
+import { SimplePriceOracle } from '../typechain/SimplePriceOracle'
+import { InterestRateModelHarness } from '../typechain/InterestRateModelHarness'
+import { Comp } from '../typechain/Comp'
+import { CompScenario } from '../typechain/CompScenario'
+import { ERC20Harness } from '../typechain/ERC20Harness'
+import { CErc20Harness } from '../typechain/CErc20Harness'
+import { expect } from './shared/expect'
+import { comptrollerFixture, bigNumber18, bigNumber17, bigNumber16 } from './shared/fixtures'
+
+const createFixtureLoader = waffle.createFixtureLoader
+
+describe('Comptroller', async () => {
+    let wallet: Wallet,
+        user1: Wallet,
+        user2: Wallet,
+        user3: Wallet,
+        user4: Wallet;
+
+    let comptroller: ComptrollerHarness
+    let priceOracle: SimplePriceOracle
+    let comp: Comp
+    let unitroller: Unitroller
+    let interestRateModel: InterestRateModelHarness
+    let usdt: ERC20Harness
+    let cusdt: CErc20Harness
+
+    let loadFixTure: ReturnType<typeof createFixtureLoader>;
+
+    before('create fixture loader', async () => {
+        [wallet, user1, user2, user3, user4] = await (ethers as any).getSigners()
+        loadFixTure = createFixtureLoader([wallet])
+        ; ({ comptroller, priceOracle, comp, unitroller, interestRateModel } = await loadFixTure(comptrollerFixture));
+
+        let testTokenFactory = await ethers.getContractFactory('ERC20Harness')
+        usdt = (await testTokenFactory.deploy(
+            bigNumber18.mul(100000000),
+            "USDT-Test",
+            18,
+            "USDT"
+        )) as ERC20Harness
+
+    })
+
+    beforeEach('deploy Comptroller', async () => {
+        const vTokenFactory = await ethers.getContractFactory('CErc20Harness')
+        cusdt = (await vTokenFactory.deploy(
+            usdt.address,
+            comptroller.address,
+            interestRateModel.address,
+            bigNumber18,
+            "CToken USDT",
+            "cUSDT",
+            BigNumber.from(18),
+            wallet.address
+        )) as CErc20Harness
+
+        await priceOracle.setUnderlyingPrice(cusdt.address, bigNumber18)
+        await comptroller._supportMarket(cusdt.address)
+    })
+
+    describe('#getMintable', async () => {
+        it('oracle', async () => {
+            expect(await comptroller.oracle()).to.eq(priceOracle.address)
+        })
+
+        it('getAssetsIn', async () => {
+            await comptroller.connect(user1).enterMarkets([cusdt.address])
+            let enteredMarkets = await comptroller.getAssetsIn(user1.address)
+            expect(enteredMarkets.length).to.eq(1)
+        })
+
+        it('getAccountSnapshot', async () => {
+            await cusdt.harnessSetBalance(user1.address, bigNumber18.mul(100))
+            let res = await cusdt.getAccountSnapshot(user1.address)
+            expect(res[0]).to.eq(0)
+            expect(res[1]).to.eq(bigNumber18.mul(100))
+            expect(res[2]).to.eq(BigNumber.from(0))
+            expect(res[3]).to.eq(bigNumber18)
+        })
+
+        it('getUnderlyingPrice', async () => {
+            expect(await priceOracle.getUnderlyingPrice(cusdt.address)).to.eq(bigNumber18)
+        })
+
+        it('getComtroller', async () => {
+            expect(await unitroller.admin()).to.eq(wallet.address)
+        })
+
+    })
+
+    describe('#mint and redeem', async () => {
+        beforeEach('each', async () => {
+            await usdt.transfer(user1.address, bigNumber18.mul(1000))
+            await usdt.approve(cusdt.address, bigNumber18.mul(100000000))
+            await usdt.connect(user1).approve(cusdt.address, bigNumber18.mul(100000000))
+        })
+
+        it('success', async () => {
+            let balance1 = await usdt.balanceOf(user1.address)
+            let cbalance1 = await cusdt.balanceOf(user1.address)
+            console.log('balance1, cbalance1:', balance1.toString(), cbalance1.toString())
+            await cusdt.connect(user1).mint(bigNumber18.mul(100))
+            let balance2 = await usdt.balanceOf(user1.address)
+            let cbalance2 = await cusdt.balanceOf(user1.address)
+            console.log('balance2, cbalance2:', balance2.toString(), cbalance2.toString())
+            expect(cbalance2).to.eq(bigNumber18.mul(100))
+
+            let rate = await cusdt.exchangeRateStored()
+            console.log('exchage rate:', rate.toString())
+
+            await cusdt.connect(user1).redeem(bigNumber18.mul(100))
+            let balance3 = await usdt.balanceOf(user1.address)
+            let cbalance3 = await cusdt.balanceOf(user1.address)
+            console.log('balance3, cbalance3:', balance3.toString(), cbalance3.toString())
+            expect(cbalance3).to.eq(bigNumber18.mul(0))
+        })
+    })
+
+    describe('#borrow and repay', async () => {
+        beforeEach('each', async () => {
+            await usdt.transfer(user1.address, bigNumber18.mul(1000))
+            await usdt.transfer(user2.address, bigNumber18.mul(1000))
+            await usdt.approve(cusdt.address, bigNumber18.mul(100000000))
+            await usdt.connect(user1).approve(cusdt.address, bigNumber18.mul(100000000))
+            await usdt.connect(user2).approve(cusdt.address, bigNumber18.mul(100000000))
+            await cusdt.connect(user1).approve(cusdt.address, ethers.constants.MaxUint256)
+            await cusdt.connect(user2).approve(cusdt.address, ethers.constants.MaxUint256)
+
+            await cusdt.connect(user1).mint(bigNumber18.mul(100))
+            let balance = await cusdt.balanceOf(user1.address)
+            expect(balance).to.eq(bigNumber18.mul(100))
+
+            await cusdt.connect(user2).mint(bigNumber18.mul(100))
+            balance = await cusdt.balanceOf(user2.address)
+            expect(balance).to.eq(bigNumber18.mul(100))
+            
+            balance = await usdt.balanceOf(cusdt.address)
+            console.log('balance:', balance.toString())
+
+            await comptroller.connect(user1).enterMarkets([cusdt.address])
+            await comptroller.connect(user2).enterMarkets([cusdt.address])
+
+        })
+
+        it('success', async () => {
+            let pbalance1 = await usdt.balanceOf(cusdt.address)
+            let balance1 = await usdt.balanceOf(user1.address)
+            let cbalance1 = await cusdt.balanceOf(user1.address)
+            console.log('pbalance1, balance1, cbalance1:', pbalance1.toString(), balance1.toString(), cbalance1.toString())
+            
+            await cusdt.connect(user1).borrow(bigNumber18.mul(10))
+            
+            let pbalance2 = await usdt.balanceOf(cusdt.address)
+            let balance2 = await usdt.balanceOf(user1.address)
+            let cbalance2 = await cusdt.balanceOf(user1.address)
+            console.log('pbalance1, balance2, cbalance2:', pbalance2.toString(), balance2.toString(), cbalance2.toString())
+            // expect(await cusdt.balanceOf(user1.address)).to.eq(BigNumber.from(0))
+        })
+
+    })
+})
---
+++ test/shared/fixtures.ts
@@ -0,0 +1,79 @@
+import { BigNumber, Wallet } from 'ethers'
+import { ethers } from 'hardhat'
+import { Unitroller } from '../../typechain/Unitroller'
+import { ComptrollerHarness } from '../../typechain/ComptrollerHarness'
+import { SimplePriceOracle } from '../../typechain/SimplePriceOracle'
+import { Comp } from '../../typechain/Comp'
+import { CompScenario } from '../../typechain/CompScenario'
+import { ERC20Harness } from '../../typechain/ERC20Harness'
+import { InterestRateModelHarness } from '../../typechain/InterestRateModelHarness'
+import { CErc20Harness } from '../../typechain/CErc20Harness'
+import { Fixture, deployMockContract, MockContract } from 'ethereum-waffle'
+
+export const bigNumber18 = BigNumber.from("1000000000000000000")  // 1e18
+export const bigNumber17 = BigNumber.from("100000000000000000")  //1e17
+export const bigNumber16 = BigNumber.from("10000000000000000")  //1e16
+export const bigNumber15 = BigNumber.from("1000000000000000")  //1e15
+export const bigNumber8 = BigNumber.from("100000000") // 1e8
+export const dateNow = BigNumber.from("1636429275") // 2021-11-09 11:41:15
+
+export async function getBlockNumber() {
+    const blockNumber = await ethers.provider.getBlockNumber()
+    // console.debug("Current block number: " + blockNumber);
+    return blockNumber;
+}
+
+interface ComptrollerFixture {
+    comptroller: ComptrollerHarness
+    priceOracle: SimplePriceOracle
+    comp: Comp
+    unitroller: Unitroller
+    interestRateModel: InterestRateModelHarness
+}
+
+export const comptrollerFixture: Fixture<ComptrollerFixture> = async function ([wallet, treasuryGuardian, treasuryAddress]: Wallet[]): Promise<ComptrollerFixture> {
+    const unitrollerFactory =  await ethers.getContractFactory('Unitroller')
+    const unitroller = (await unitrollerFactory.deploy()) as Unitroller
+    const comptrollerFactory = await ethers.getContractFactory('ComptrollerHarness');
+    const comptroller = (await comptrollerFactory.deploy()) as ComptrollerHarness
+
+    const priceOracleFactory = await ethers.getContractFactory('SimplePriceOracle')
+    const priceOracle = (await priceOracleFactory.deploy()) as SimplePriceOracle
+
+    const closeFactor = bigNumber17.mul(6)
+    const liquidationIncentive = BigNumber.from('1080000000000000000')
+
+    const compFactory = await ethers.getContractFactory('Comp')
+    const comp = (await compFactory.deploy(wallet.address)) as Comp
+
+    const compRate = bigNumber18
+
+
+    await unitroller._setPendingImplementation(comptroller.address)
+    await comptroller._become(unitroller.address)
+
+    await comptroller._setLiquidationIncentive(liquidationIncentive)
+    // console.log('=========_setLiquidationIncentive==========')
+    await comptroller._setCloseFactor(closeFactor)
+    // console.log('=========_setCloseFactor==========')
+    await comptroller._setPriceOracle(priceOracle.address)
+    // console.log('=========_setPriceOracle==========')
+    await comptroller.setCompAddress(comp.address)
+    // console.log('=========setCompAddress==========')
+
+    await comptroller.harnessSetCompRate(compRate)
+    // console.log('=========harnessSetVenusRate==========')
+
+
+    const interestRateModelHarnessFactory = await ethers.getContractFactory('InterestRateModelHarness')
+    const interestRateModel = (await interestRateModelHarnessFactory.deploy(BigNumber.from(0))) as InterestRateModelHarness
+
+    console.log(`
+        comptroller: ${comptroller.address},
+        priceOracle: ${priceOracle.address},
+        comp: ${comp.address},
+        unitroller: ${unitroller.address},
+        interestRateModel: ${interestRateModel.address},
+    `)
+    return { comptroller, priceOracle, comp, unitroller, interestRateModel };
+}
